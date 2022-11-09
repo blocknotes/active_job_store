d1f05221893b65d07a31cafdbf77b220eed84617
blocknotes <mat@blocknot.es>
2022-11-09 23:06:54 +0100

    test: RSpec extra options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
+  config.color = true
+  config.disable_monkey_patching!
+  config.order = :random
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.tty = true
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -8,6 +14,4 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
 end
